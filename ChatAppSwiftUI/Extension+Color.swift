12616500f360778b5bd50a1af549b5193af57730
unicorn <122259771+Unicorn8080@users.noreply.github.com>
2023-08-23 12:44:13 +0300

    Hex color extension added.

--- ChatAppSwiftUI/Extension+Color.swift
+++ ChatAppSwiftUI/Extension+Color.swift
@@ -5,4 +5,24 @@
 //  Created by Bahadır Tarcan on 23.08.2023.
 //
 
-import Foundation
+import SwiftUI
+
+
+extension Color {
+    init(hex: String) {
+        var cString: String = hex.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
+
+        if cString.hasPrefix("#") {
+            cString.remove(at: cString.startIndex)
+        }
+
+        var rgbValue: UInt64 = 0
+        Scanner(string: cString).scanHexInt64(&rgbValue)
+
+        self.init(
+            red: Double((rgbValue & 0xFF0000) >> 16) / 255.0,
+            green: Double((rgbValue & 0x00FF00) >> 8) / 255.0,
+            blue: Double(rgbValue & 0x0000FF) / 255.0
+        )
+    }
+}
