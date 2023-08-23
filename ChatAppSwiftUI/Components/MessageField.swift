31768bfc5fe391c78905572231bd067988c0fe22
unicorn <122259771+Unicorn8080@users.noreply.github.com>
2023-08-23 13:26:01 +0300

    MessageField chat adding.

--- ChatAppSwiftUI/Components/MessageField.swift
+++ ChatAppSwiftUI/Components/MessageField.swift
@@ -8,13 +8,49 @@
 import SwiftUI
 
 struct MessageField: View {
+    @State private var message = ""
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            CustomTextField(placeholder: Text("Enter your message here"), text: $message)
+            
+            Button {
+                
+            } label: {
+                Image(systemName: "paperplane.fill")
+                    .foregroundColor(.white)
+                    .padding(10)
+                    .background(Color(hex: "FFD8C4"))
+                    .cornerRadius(50)
+            }
+        }
+        .padding(.horizontal)
+        .padding(.vertical, 10)
+        .background(Color(hex: "F5F5F5"))
+        .cornerRadius(50)
+        .padding()
     }
 }
 
 struct MessageField_Previews: PreviewProvider {
     static var previews: some View {
         MessageField()
+    }
+}
+
+struct CustomTextField: View {
+    var placeholder: Text
+    @Binding var text: String
+    var editingChanged: (Bool) -> () = {_ in}
+    var commit: () -> () = {}
+    
+    var body: some View {
+        ZStack(alignment: .leading) {
+            if text.isEmpty {
+                placeholder
+                    .padding(0.5)
+            }
+            
+            TextField("", text: $text, onEditingChanged: editingChanged, onCommit: commit)
+        }
     }
 }
