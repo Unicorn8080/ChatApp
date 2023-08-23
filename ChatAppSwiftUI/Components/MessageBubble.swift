02a9cd14e5c879c4e7fc0951dc8455f4a400b19d
unicorn <122259771+Unicorn8080@users.noreply.github.com>
2023-08-23 13:10:24 +0300

    Bubble UI added.

---
+++ ChatAppSwiftUI/Components/MessageBubble.swift
@@ -0,0 +1,44 @@
+//
+//  MessageBubble.swift
+//  ChatAppSwiftUI
+//
+//  Created by Bahadır Tarcan on 23.08.2023.
+//
+
+import SwiftUI
+
+struct MessageBubble: View {
+    var message: Message
+    @State private var showTime = false
+    
+    var body: some View {
+        VStack(alignment: message.received ? .leading : .trailing) {
+            HStack {
+                Text(message.text)
+                    .padding()
+                    .background(message.received ?
+                                Color.gray : Color(hex: "FFE5B4"))
+                    .cornerRadius(30)
+            }
+            .frame(maxWidth: 300, alignment: message.received ? .leading : .trailing)
+            .onTapGesture {
+                showTime.toggle()
+            }
+            if showTime {
+                Text("\(message.timestamp.formatted(.dateTime.hour().minute()))")
+                    .font(.caption2)
+                    .foregroundColor(Color.gray)
+                    .padding(message.received ? .leading : .trailing, 25)
+            }
+        }
+        .frame(maxWidth: .infinity, alignment: message.received ? .leading : .trailing)
+        .padding(message.received ? .leading : .trailing)
+        .padding(.horizontal, 10)
+    }
+}
+
+struct MessageBubble_Previews: PreviewProvider {
+    static var previews: some View {
+        MessageBubble(message: Message(id: "12345", text: "I've been coding SwiftUI applications from scratch and it's so much fun!", received: true, timestamp: Date()))
+    }
+}
