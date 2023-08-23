e62e90c7a4791e826594decb84792f803a16e2b8
unicorn <122259771+Unicorn8080@users.noreply.github.com>
2023-08-23 12:44:25 +0300

    TitleRow finish.

--- ChatAppSwiftUI/Components/TitleRow.swift
+++ ChatAppSwiftUI/Components/TitleRow.swift
@@ -8,13 +8,44 @@
 import SwiftUI
 
 struct TitleRow: View {
+    var imageUrl = URL(string: "https://images.unsplash.com/photo-1531746020798-e6953c6e8e04?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2550&q=80")
+    var name = "Ayşe Fatma"
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack(spacing: 20) {
+            AsyncImage(url: imageUrl) { image in
+                image.resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 50, height: 50)
+                    .cornerRadius(50)
+            } placeholder: {
+                ProgressView()
+            }
+            
+            VStack(alignment: .leading) {
+                Text(name)
+                    .font(.title).bold()
+                
+                Text("Online")
+                    .font(.caption)
+                    .foregroundColor(.gray)
+            }
+            .frame(maxWidth: .infinity, alignment: .leading)
+            Image(systemName: "phone.fill")
+                .foregroundColor(.gray)
+                .padding(10)
+                .background(.white)
+                .cornerRadius(50)
+        }
+        
+        .padding()
+        
     }
 }
 
 struct TitleRow_Previews: PreviewProvider {
     static var previews: some View {
         TitleRow()
+            .background(Color(hex: "FFD8C4"))
     }
 }
