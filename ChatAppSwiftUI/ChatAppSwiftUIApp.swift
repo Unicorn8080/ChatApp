e7b4f84bdc5a1aa0117c7fd422480869194d79b7
unicorn <122259771+Unicorn8080@users.noreply.github.com>
2023-08-23 12:01:43 +0300

    Initial Commit

---
+++ ChatAppSwiftUI/ChatAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAppSwiftUIApp.swift
+//  ChatAppSwiftUI
+//
+//  Created by Bahadır Tarcan on 23.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct ChatAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
