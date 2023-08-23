33aa9f187243f9db2e03fe16bd99dee52789240b
unicorn <122259771+Unicorn8080@users.noreply.github.com>
2023-08-23 13:14:13 +0300

    MessageField folder added.

---
+++ ChatAppSwiftUI/Components/MessageField.swift
@@ -0,0 +1,20 @@
+//
+//  MessageField.swift
+//  ChatAppSwiftUI
+//
+//  Created by Bahadır Tarcan on 23.08.2023.
+//
+
+import SwiftUI
+
+struct MessageField: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MessageField_Previews: PreviewProvider {
+    static var previews: some View {
+        MessageField()
+    }
+}
