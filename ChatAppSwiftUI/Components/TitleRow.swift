3ae9d3e8d85d4b140ed4c37b6dc6751bdf004cd9
unicorn <122259771+Unicorn8080@users.noreply.github.com>
2023-08-23 12:27:34 +0300

    TitleRow folder added.

---
+++ ChatAppSwiftUI/Components/TitleRow.swift
@@ -0,0 +1,20 @@
+//
+//  TitleRow.swift
+//  ChatAppSwiftUI
+//
+//  Created by Bahadır Tarcan on 23.08.2023.
+//
+
+import SwiftUI
+
+struct TitleRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TitleRow_Previews: PreviewProvider {
+    static var previews: some View {
+        TitleRow()
+    }
+}
