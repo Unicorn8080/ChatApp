d51aadba7c197b6da24583c136baee770ec2a973
unicorn <122259771+Unicorn8080@users.noreply.github.com>
2023-08-24 18:12:49 +0300

    UserDefaults added.

--- ChatAppSwiftUI/Models/ChatModel.swift
+++ ChatAppSwiftUI/Models/ChatModel.swift
@@ -27,9 +27,49 @@ class ChatModel: ObservableObject {
         
         DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
             let randomIndex = Int.random(in: 0..<self.possibleReplies.count)
-            let randomReply = self.possibleReplies[randomIndex]
-            let botMessage = Message(content: randomReply, isUser: false)
-            self.messages.append(botMessage)
+                   let randomReply = self.possibleReplies[randomIndex]
+                   let botMessage = Message(content: randomReply, isUser: false)
+                   self.messages.append(botMessage)
+                   
+                   let userMessage = Message(content: message, isUser: true)
+                   
+                   
+                   self.saveMessagesToUserDefaults()
+            
+            
         }
     }
-}
+    
+  
+        
+        private func saveMessagesToUserDefaults() {
+            do {
+                let encodedMessages = try JSONEncoder().encode(messages)
+                UserDefaults.standard.set(encodedMessages, forKey: "messages")
+            } catch {
+                print("Error saving messages to UserDefaults: \(error)")
+            }
+        }
+        
+        private func loadMessagesFromUserDefaults() {
+            if let encodedMessages = UserDefaults.standard.data(forKey: "messages") {
+                do {
+                    let decodedMessages = try JSONDecoder().decode([Message].self, from: encodedMessages)
+                    messages = decodedMessages
+                } catch {
+                    print("Error loading messages from UserDefaults: \(error)")
+                }
+            }
+        }
+        
+        init() {
+            loadMessagesFromUserDefaults()
+        }
+    }
+    
+
+
+
+
+
+
